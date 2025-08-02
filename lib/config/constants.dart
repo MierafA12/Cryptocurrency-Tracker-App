f69f195b421834a0195ccf00d797b345eba78ec7
MierafA12 <mierafabebe12@gmail.com>
2025-08-02 18:35:08 +0300

    adding constants widget

---
+++ lib/config/constants.dart
@@ -0,0 +1,3 @@
+const String baseUrl = 'https://api.coingecko.com/api/v3';
+const String marketEndpoint = '/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1';
+const String coinDetailEndpoint = '/coins'; // append /{id}
