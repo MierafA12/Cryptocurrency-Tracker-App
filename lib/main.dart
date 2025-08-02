0b6e79e4bf51b7eb1ff28b4a8c3ff02b485d2826
MierafA12 <mierafabebe12@gmail.com>
2025-08-02 18:07:58 +0300

    adding route

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/crypto_provider.dart';
 import './screens/onbording_screen.dart';
+import './screens/home_screen.dart'; // Import your HomeScreen
 
 void main() {
   runApp(
@@ -9,10 +10,22 @@ void main() {
       providers: [
         ChangeNotifierProvider(create: (_) => CryptoProvider()),
       ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: OnbordingScreen(),
-      ),
+      child: MyApp(),
     ),
   );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => OnbordingScreen(),
+        '/home': (context) => HomeScreen(),
+        // Add more routes as needed
+      },
+    );
+  }
 }
